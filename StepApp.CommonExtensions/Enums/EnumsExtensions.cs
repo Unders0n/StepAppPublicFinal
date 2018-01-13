84cfca573ac03449141373b6ecf5dbc3cb955dd8
Andrey Stepanov <web@pro-log.ru>
2018-01-13 17:32:38 +0300

    new enum extensions for handling descriptions

--- StepApp.CommonExtensions/Enums/EnumsExtensions.cs
+++ StepApp.CommonExtensions/Enums/EnumsExtensions.cs
@@ -5,9 +5,6 @@ using System.Data.Entity;
 using System.Data.Entity.Migrations;
 using System.Linq;
 
-//test 5
-//test new
-//test 6
 namespace StepApp.CommonExtensions.Enums
 {
     public static class EnumExtensions
@@ -19,6 +16,37 @@ namespace StepApp.CommonExtensions.Enums
                    .Cast<DescriptionAttribute>()
                    .FirstOrDefault()?.Description ?? string.Empty;
 
+        /// <summary>
+        /// Get list of text descriptions (by DescriptionAttribute)
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="values">Enum values one by one</param>
+        /// <returns></returns>
+        public static List<string> GetEnumDescriptions<TEnum>(params TEnum[] values)
+        {
+            var lst = values.ToList();
+            return EnumDescriptions(lst);
+        }
+
+        /// <summary>
+        /// Get list of text descriptions (by DescriptionAttribute)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumeration">Enumeration type</param>
+        /// <returns></returns>
+        public static List<string> ToListDescriptions<T>(this Type enumeration)
+        {
+            var lst = Enum.GetValues(enumeration).Cast<T>().ToList();
+            return EnumDescriptions(lst);
+        }
+
+        private static List<string> EnumDescriptions<TEnum>(List<TEnum> lst)
+        {
+            List<string> descriptionCollection = new List<string>();
+            foreach (var choice in lst) descriptionCollection.Add(choice.GetEnumDescription());
+            return descriptionCollection;
+        }
+
         public static void SeedEnumValues<T, TEnum>(this IDbSet<T> dbSet, Func<TEnum, T> converter)
             where T : class => Enum.GetValues(typeof(TEnum))
                                    .Cast<object>()
@@ -29,6 +57,17 @@ namespace StepApp.CommonExtensions.Enums
         public static List<T> ToList<T>(this Type enumeration)
         {
           return  Enum.GetValues(enumeration).Cast<T>().ToList();
+        }
+        
+        /// <summary>
+        /// Find enum value in given enum type by description string
+        /// </summary>
+        /// <typeparam name="TEnum">enum type</typeparam>
+        /// <param name="str">String to find in corresponding Description</param>
+        /// <returns></returns>
+        public static TEnum FindEnumValueByDescriptionString<TEnum>(this string str)
+        {
+            return typeof(TEnum).ToList<TEnum>().FirstOrDefault(s => s.GetEnumDescription() == str);
         }
     }
 }
